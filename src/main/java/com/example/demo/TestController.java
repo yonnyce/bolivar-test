cc05e7e0679f34d7db4ebb32db59a26b0edac3ac
Jonatan Quiroz Cuadrado <jonatan.quirozc@perficient.com>
2022-08-17 08:29:42 -0500

    Test controller added

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testRequest(){
+        return ResponseEntity.ok("Holi");
+    }
+}
